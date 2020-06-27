fbdb02351d53d653b791c9877d443f6e7163567b
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-27 20:02:19 +0530

    updated type resolvers

--- api/packages/graphql/schema/post/resolvers/types/Post.ts
+++ api/packages/graphql/schema/post/resolvers/types/Post.ts
@@ -1,7 +1,16 @@
 /**
  * Post type resolvers
  */
+import { User } from '@prisma/client';
+import { Parent, Args, Context } from '../../../../types';
 
-const Post = {};
+const Post = {
+  author: async (parent: Parent, _: Args, context: Context): Promise<User | null> => {
+    const { id } = parent;
+    const { prisma } = context;
+
+    return prisma.post.findOne({ where: { id } }).author();
+  }
+};
 
 export default Post;
--- api/packages/graphql/schema/user/resolvers/types/User.ts
+++ api/packages/graphql/schema/user/resolvers/types/User.ts
@@ -1,7 +1,16 @@
 /**
  * User type resolvers
  */
+import { Post } from '@prisma/client';
+import { Parent, Args, Context } from '../../../../types';
 
-const User = {};
+const User = {
+  posts: async (parent: Parent, _: Args, context: Context): Promise<Post[] | null> => {
+    const { id } = parent;
+    const { prisma } = context;
+
+    return prisma.user.findOne({ where: { id } }).posts();
+  }
+};
 
 export default User;
