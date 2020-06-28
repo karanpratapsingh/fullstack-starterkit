9cd237e39e9063badcf421fea748603b843161df
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-28 12:08:27 +0530

    added graphql test

---
+++ api/tests/graphql/graphql.test.ts
@@ -0,0 +1,38 @@
+import TestSuiteUtils, { TestSuiteType } from '../utils';
+import { GraphQLApi } from '../../packages/graphql';
+
+const utils = new TestSuiteUtils(TestSuiteType.GRAPHQL);
+
+let graphQLApi: GraphQLApi;
+
+beforeAll(() => {
+  graphQLApi = utils.graphQLApi;
+});
+
+describe('GraphQL Test Suite', () => {
+  describe('getUser query', () => {
+    test('works with valid input', async () => {
+      const id = 'demo_user_id';
+      const { user } = await graphQLApi.getUser({ id });
+
+      if (user) {
+        expect(user.id).toBe(id);
+      } else {
+        expect(user).toBe(null);
+      }
+    });
+  });
+
+  describe('getPost query', () => {
+    test('works with valid input', async () => {
+      const id = 'demo_post_id';
+      const { post } = await graphQLApi.getPost({ id });
+
+      if (post) {
+        expect(post.id).toBe(id);
+      } else {
+        expect(post).toBe(null);
+      }
+    });
+  });
+});
