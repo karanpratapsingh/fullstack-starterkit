60a7278109222c668450a130d21e24494235217c
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-06 18:27:41 +0530

    update theme files

---
+++ web/src/theme/palette.ts
@@ -0,0 +1,16 @@
+const palette = {
+  primary: {
+    light: '#D1C4E9',
+    main: '#673AB7',
+    dark: '#512DA8'
+  },
+  secondary: {
+    main: '#00BCD4'
+  },
+  text: {
+    primary: '#212121',
+    secondary: '#757575'
+  }
+};
+
+export default palette;
