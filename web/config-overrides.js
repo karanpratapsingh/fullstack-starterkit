4211a33430a21dc81a8b9958b26208ea058974f7
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-07 12:11:46 +0530

    added react-app-rewired

---
+++ web/config-overrides.js
@@ -0,0 +1,9 @@
+const { alias, configPaths } = require('react-app-rewire-alias');
+
+module.exports = function override(config) {
+  alias({
+    ...configPaths('tsconfig.paths.json')
+  })(config);
+
+  return config;
+};
