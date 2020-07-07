bb348277d18f9c71d9c5edf8f3c3a5b8bee79c28
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-07 15:03:52 +0530

    added config

---
+++ web/src/config/index.ts
@@ -0,0 +1,21 @@
+const { version } = require('../../package.json');
+
+class Config {
+  version!: string;
+  endpoints!: {
+    https: string;
+    wss: string;
+  };
+
+  constructor(env: NodeJS.ProcessEnv) {
+    Object.assign(this, {
+      version,
+      endpoints: {
+        https: env.REACT_APP_ENDPOINT_HTTPS,
+        wss: env.REACT_APP_ENDPOINT_WSS
+      }
+    });
+  }
+}
+
+export default Config;
