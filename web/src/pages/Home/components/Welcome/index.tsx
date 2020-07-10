1f0d4e2bc205fcd690c025d8258708805c7c75ed
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-10 18:56:10 +0530

    added welcome component

---
+++ web/src/pages/Home/components/Welcome/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactLogo from '@web/assets/images/logo.png';
+import { Container, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(({ palette, typography, spacing, breakpoints }) => ({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  logo: {
+    width: '40%',
+    [breakpoints.down('sm')]: {
+      width: '60%'
+    },
+    [breakpoints.down('xs')]: {
+      width: '80%'
+    }
+  },
+  title: {
+    color: palette.grey[800],
+    fontWeight: typography.fontWeightRegular,
+    marginTop: spacing(4)
+  },
+  subtitle: {
+    color: palette.grey[700],
+    fontWeight: typography.fontWeightLight,
+    marginTop: spacing(2)
+  },
+  body: {
+    color: palette.grey[700],
+    fontWeight: typography.fontWeightLight,
+    marginTop: spacing(1)
+  }
+}));
+
+function Welcome(): React.ReactElement<{}> {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container}>
+      <img className={classes.logo} src={ReactLogo} alt='starterkit' />
+      <Typography variant='h4' className={classes.title}>
+        Full Stack Starterkit
+      </Typography>
+      <Typography variant='body2' className={classes.subtitle}>
+        GraphQL first starter kit that scales
+      </Typography>
+      <Typography variant='caption' className={classes.body}>
+        powered by TypeScript
+      </Typography>
+    </Container>
+  );
+}
+
+export default Welcome;
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,45 +1,15 @@
 import React from 'react';
-import ReactLogo from '@web/assets/images/logo.png';
-import { Container, Typography } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Welcome from './components/Welcome';
 
-const useStyles = makeStyles(({ palette, typography, spacing, breakpoints }) => ({
+const useStyles = makeStyles(() => ({
   container: {
     display: 'flex',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'column'
-  },
-  welcomeContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column'
-  },
-  logo: {
-    width: '40%',
-    [breakpoints.down('sm')]: {
-      width: '60%'
-    },
-    [breakpoints.down('xs')]: {
-      width: '80%'
-    }
-  },
-  title: {
-    color: palette.grey[800],
-    fontWeight: typography.fontWeightRegular,
-    marginTop: spacing(4)
-  },
-  subtitle: {
-    color: palette.grey[700],
-    fontWeight: typography.fontWeightLight,
-    marginTop: spacing(2)
-  },
-  body: {
-    color: palette.grey[700],
-    fontWeight: typography.fontWeightLight,
-    marginTop: spacing(1)
   }
 }));
 
@@ -47,19 +17,8 @@ function Home(): React.ReactElement<{}> {
   const classes = useStyles();
 
   return (
-    <Container className={classes.container}>
-      <Container className={classes.welcomeContainer}>
-        <img className={classes.logo} src={ReactLogo} alt='starterkit' />
-        <Typography variant='h4' className={classes.title}>
-          Full Stack Starterkit
-        </Typography>
-        <Typography variant='body2' className={classes.subtitle}>
-          GraphQL first starter kit that scales
-        </Typography>
-        <Typography variant='caption' className={classes.body}>
-          powered by TypeScript
-        </Typography>
-      </Container>
+    <Container className={classes.container} maxWidth='xl'>
+      <Welcome />
     </Container>
   );
 }
