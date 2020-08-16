25c8213861acd5df5ec853a7dd3b80e60bd693da
Karan Pratap Singh <synester.gates4@gmail.com>
2020-08-16 14:12:57 +0530

    minor prisma fix

--- backend/tests/utils/index.ts
+++ backend/tests/utils/index.ts
@@ -41,7 +41,7 @@ class TestSuiteUtils {
 
   private afterAll = async (done: () => void): Promise<void> => {
     await this.cleanup();
-    await this.prisma.disconnect();
+    await this.prisma.$disconnect();
     done();
   };
 
