e9e59fde79ba8c46ebdb35e5438778607983dfd3
Karan Pratap Singh <synester.gates4@gmail.com>
2020-10-11 13:28:51 +0530

    feat(backend): added subscriptions support :zap:

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,24 @@
 import './packages/utils/env';
-import express from 'express';
+import http from 'http';
+import express, { Express } from 'express';
 import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
 import { GraphQLServerOptions } from '@backend/graphql';
 import { logger } from '@backend/utils';
 import Config from '@backend/config';
 
-const app = express();
+const app: Express = express();
 app.use(cors());
 
-const PORT = process.env.PORT || Config.defaultPort;
-const server = new ApolloServer(GraphQLServerOptions);
+const PORT: string | number = process.env.PORT || Config.defaultPort;
+const server: ApolloServer = new ApolloServer(GraphQLServerOptions);
 server.applyMiddleware({ app });
 
-app.listen(PORT, () => {
-  logger.info('GraphQL Server is running 🚀');
+const httpServer: http.Server = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
+  const { graphqlPath, subscriptionsPath } = server;
+  logger.info(`🚀 GraphQL Server is running at http://localhost:${PORT}${graphqlPath}`);
+  logger.info(`🚀 Subscriptions ready at ws://localhost:${PORT}${subscriptionsPath}`);
 });
