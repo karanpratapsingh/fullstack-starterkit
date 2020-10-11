d1c293f335c2dcb3080779f264ff6f04cf4b8dac
Karan Pratap Singh <synester.gates4@gmail.com>
2020-10-11 13:29:09 +0530

    fixes(backend): minor fixes

--- backend/packages/utils/index.ts
+++ backend/packages/utils/index.ts
@@ -1,9 +1,9 @@
 import cuid from 'cuid';
 
-const generateId = (prefix?: string): string => {
+function generateId(prefix?: string): string {
   const generatedId = cuid();
   return `${prefix}${generatedId}`;
-};
+}
 
 export { generateId };
 export * from './logger';
