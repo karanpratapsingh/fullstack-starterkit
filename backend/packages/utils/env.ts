dcbf41b81b63d006f79ef8c2c0635c3c5f112f9a
Karan Pratap Singh <synester.gates4@gmail.com>
2020-10-11 13:35:41 +0530

    fixes(env): minor

--- backend/packages/utils/env.ts
+++ backend/packages/utils/env.ts
@@ -1,3 +1,7 @@
+/**
+ * ENV Configuration
+ * Make sure to configure your environment variables while in production.
+ */
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
